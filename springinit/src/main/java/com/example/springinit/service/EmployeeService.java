37523fbd46a3fe9e886cf3941fb651ea3d70fb97
hailayw <hailaygidey5@gmail.com>
2022-06-04 20:38:51 -0500

    service updated

--- springinit/src/main/java/com/example/springinit/service/EmployeeService.java
+++ springinit/src/main/java/com/example/springinit/service/EmployeeService.java
@@ -1,7 +1,12 @@
 package com.example.springinit.service;
 
+import com.example.springinit.entity.Employee;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+
+    public String getEmployee() {
+        return "Employee-A";
+    }
 }
