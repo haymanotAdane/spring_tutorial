975a84662f8845a0f23f6c177a54d53cecf369ad
haymanotAdane <haymanotadaneyimam@gmail.com>
2022-06-04 20:23:43 -0500

    updating service

--- springinit/src/main/java/com/example/springinit/service/EmployeeService.java
+++ springinit/src/main/java/com/example/springinit/service/EmployeeService.java
@@ -1,7 +1,19 @@
 package com.example.springinit.service;
 
+import com.example.springinit.entity.Employee;
+import com.example.springinit.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
+    @Autowired
+    EmployeeRepository empRepo;
+
+    public List<Employee> getEmployees(){
+        return empRepo.findAll();
+
+    }
 }
