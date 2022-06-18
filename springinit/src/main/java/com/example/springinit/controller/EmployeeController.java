0d16c3d006049010fac8e80ad2a3c8ca11cf704b
hailayw <hailaygidey5@gmail.com>
2022-06-18 18:24:54 -0500

    rearranged

--- springinit/src/main/java/com/example/springinit/controller/EmployeeController.java
+++ springinit/src/main/java/com/example/springinit/controller/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.example.springinit.controller;
 
 import com.example.springinit.entity.Employee;
-import com.example.springinit.repository.EmployeeRepository;
 import com.example.springinit.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
